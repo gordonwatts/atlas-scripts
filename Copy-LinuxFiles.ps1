5adc5121db4f98cef82c7e7c01fa8fcbecd9c00e
Gordon Watts <gwatts@uw.edu>
2023-11-05 12:31:22 -0800

    Move over grid cert files!

--- Copy-LinuxFiles.ps1
+++ Copy-LinuxFiles.ps1
@@ -48,3 +48,31 @@ foreach ($file in $allFiles) {
         wsl -d $DistroName chmod 600 ~/.ssh/$($file.Name)
     }
 }
+
+# Now prepare the .globus files. They are in the OneDrive foler "CERNCert"
+# It should be the only .p12 file in the directory
+$CERNCert = "$OneDrive\.ssh\CERNCert"
+if (-not $(Test-Path $CERNCert)) {
+    Write-Host "CERN Certificates not found at $CERNCert"
+    exit
+}
+$p12Files = Get-ChildItem -Path $CERNCert -Filter "*.p12"
+if ($p12Files.Count -ne 1) {
+    Write-Host "Found $($p12Files.Count) .p12 files in $CERNCert. There should be only one."
+    exit
+}
+$p12File = $p12Files[0]
+
+wsl -d $DistroName mkdir -p ~/.globus
+$wslFIle = ConvertTo-WslPath $p12File.FullName
+wsl -d $DistroName cp $wslFile ~/.globus/myCert.p12
+
+Write-Host "Installing Globus certificates. You'll be prompted for the pass phrase for your p12 file"
+Write-Host "and then a pass phrase for the userkey.pem file."
+wsl -d $DistroName openssl pkcs12 -nocerts -in ~/.globus/myCert.p12 -out ~/.globus/userkey.pem
+Write-Host "And again for the p12 file to generate the usercert.pem"
+wsl -d $DistroName openssl pkcs12 -clcerts -nokeys -in ~/.globus/myCert.p12 -out ~/.globus/usercert_noText.pem
+wsl -d $DistroName bash -c "openssl x509 -in ~/.globus/usercert_noText.pem -text > ~/.globus/usercert.pem"
+wsl -d $DistroName rm ~/.globus/usercert_noText.pem
+wsl -d $DistroName chmod 444 ~/.globus/usercert.pem
+wsl -d $DistroName chmod 400 ~/.globus/userkey.pem
